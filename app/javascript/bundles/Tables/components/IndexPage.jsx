a0e2fae7172faecb037a098ba3945fbcda268190
Thomas Wilson <twilson.spqr@Sleipnir.local>
2017-09-28 11:53:11 -0400

    heroku

--- app/javascript/bundles/Tables/components/IndexPage.jsx
+++ app/javascript/bundles/Tables/components/IndexPage.jsx
@@ -13,13 +13,13 @@ class IndexPage extends Component {
               <th></th>
             </tr>
           </thead>
- 
+
           <tbody>
               <tr>
                 <td>{table.name}</td>
                 <td><a href={"/tables/"+table.id}>Show</a></td>
                 <td><a rel="nofollow" data-method="delete" href={"/tables/"+table.id+"/invitations"}>Leave</a></td>
-                <td><a href={"/tables/"+table.id+""/invitations/new"}>Invitations</a></td>
+                <td><a href={"/tables/"+table.id+"/invitations/new"}>Invitations</a></td>
               </tr>
           </tbody>
         </table>
